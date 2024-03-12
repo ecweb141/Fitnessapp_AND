66f8e0618029098b8a45934d871a2e52f8c0427d
chetan gavit <chetan@windows.windows>
2024-03-12 19:37:53 +0530

    First Deploy

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Its' not Hello world for every one</Text>
+      <Link href={'/Dashboard'}>GO TO Dashboard</Link>
       <StatusBar style="auto" />
     </View>
   );
